88cfb683d7c1186a1ccfc9494b422b6a04da91e1
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-10 19:35:50 -0800

    redid assignment the correct way

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,8 +5,12 @@ end
 
 post '/color' do
 
+  content_type :json
+
   #Create and return a JSON object with the random cell and color given below.
 
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+
+  {cell: cell, color: color}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('a#get_color').on('click', function(e) {
     e.preventDefault()
     var random_num = Math.floor((Math.random()*9)+1)
-    var num = Math.floor((Math.random()* 0xffffff)+1).toString().slice(0,6)
-    var color = '#' + num
-    // color= "#" + "%06x" % (rand * 0xffffff)
-    $.ajax({
-      url: '/color',
-      method: 'post',
-      data: {submit: true}
-    }).done(function(server_data) {
-      $('li:nth-child(' + random_num +')').css('background-color', color)
+
+    $.post('/color', {
+      dataType: 'json'
+    }, function(data, status) {
+      $('li:nth-child(' + data.cell + ')').css('background-color', data.color)
     })
   })
-});
+})
